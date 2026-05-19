f1fa4adfc4d1994b1c4c9d60e148cbf36a6800ed
Wemmi787 <duselwin1@outlook.com>
2026-05-19 17:35:04 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,17 @@
-local http = require('http')
+local socket = require("socket")
+local server = assert(socket.bind("*", 8080))
+print("Luau server running on port 8080")
 
-http.createServer(function (req, res)
-  res:setHeader("Content-Type", "text/plain")
-  res:finish("Lua server running on Railway!")
-end):listen(8080)
+while true do
+    local client = server:accept()
+    client:settimeout(1)
 
-print("Server running on port 8080")
+    local line = client:receive()
+    if line then
+        client:send("HTTP/1.1 200 OK\r\n")
+        client:send("Content-Type: text/plain\r\n\r\n")
+        client:send("Luau server running on Railway!")
+    end
+
+    client:close()
+end
