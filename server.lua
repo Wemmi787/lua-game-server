4244fcdf10f88d616b1e59457d21c29270c10d14
Wemmi787 <duselwin1@outlook.com>
2026-05-19 17:56:53 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,16 +1,36 @@
 local socket = require("socket")
+
 local server = assert(socket.bind("*", 8080))
-print("Lua server running on port 8080")
+print("Lua API server running on port 8080")
+
+local function read_request(client)
+    local line = client:receive()
+    if not line then return nil end
+
+    local method, path = line:match("^(%w+)%s+(.-)%s+HTTP")
+    return method, path
+end
+
+local function send(client, body, contentType)
+    client:send("HTTP/1.1 200 OK\r\n")
+    client:send("Content-Type: " .. (contentType or "text/plain") .. "\r\n\r\n")
+    client:send(body)
+end
 
 while true do
     local client = server:accept()
     client:settimeout(1)
 
-    local line = client:receive()
-    if line then
-        client:send("HTTP/1.1 200 OK\r\n")
-        client:send("Content-Type: text/plain\r\n\r\n")
-        client:send("Lua server running on Railway!")
+    local method, path = read_request(client)
+
+    if path == "/" then
+        send(client, "Lua server running")
+    elseif path == "/move" then
+        send(client, "Move received")
+    elseif path == "/state" then
+        send(client, "Game state here")
+    else
+        send(client, "Not found", "text/plain")
     end
 
     client:close()
