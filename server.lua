d3aff251de450fb7943b7ee2ba291945cd480ef2
Wemmi787 <duselwin1@outlook.com>
2026-05-19 17:39:42 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 local socket = require("socket")
 local server = assert(socket.bind("*", 8080))
-print("Luau server running on port 8080")
+print("Lua server running on port 8080")
 
 while true do
     local client = server:accept()
@@ -10,7 +10,7 @@ while true do
     if line then
         client:send("HTTP/1.1 200 OK\r\n")
         client:send("Content-Type: text/plain\r\n\r\n")
-        client:send("Luau server running on Railway!")
+        client:send("Lua server running on Railway!")
     end
 
     client:close()
