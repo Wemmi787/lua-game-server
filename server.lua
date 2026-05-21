372fc6716d26d172d9b126a3ecd7e9e2e26bac68
Wemmi787 <duselwin1@outlook.com>
2026-05-21 18:06:28 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,37 +1,38 @@
 local socket = require("socket")
-
 local server = assert(socket.bind("*", 8080))
-print("Lua API server running on port 8080")
+server:settimeout(0)
 
-local function read_request(client)
-    local line = client:receive()
-    if not line then return nil end
+local queues = {}  -- queues[serverID] = { list of requests }
 
-    local method, path = line:match("^(%w+)%s+(.-)%s+HTTP")
-    return method, path
-end
-
-local function send(client, body, contentType)
-    client:send("HTTP/1.1 200 OK\r\n")
-    client:send("Content-Type: " .. (contentType or "text/plain") .. "\r\n\r\n")
-    client:send(body)
-end
-
-while true do
+-- receive messages
+local function receiveMessages()
     local client = server:accept()
-    client:settimeout(1)
-
-    local method, path = read_request(client)
-
-    if path == "/" then
-        send(client, "Lua server running")
-    elseif path == "/move" then
-        send(client, "Move received")
-    elseif path == "/state" then
-        send(client, "Game state here")
-    else
-        send(client, "Not found", "text/plain")
+    if client then
+        client:settimeout(1)
+        local msg = client:receive("*l")
+        if msg then
+            local serverID = msg:match("^(.-)|")
+            queues[serverID] = queues[serverID] or {}
+            table.insert(queues[serverID], msg)
+        end
+        client:close()
     end
+end
 
-    client:close()
+-- send batches every second
+local function sendBatches()
+    for serverID, list in pairs(queues) do
+        if #list > 0 then
+            -- send to host (HTTP POST or socket)
+            sendToHost(serverID, table.concat(list, "\n"))
+            queues[serverID] = {}
+        end
+    end
+end
+
+-- main loop
+while true do
+    receiveMessages()
+    sendBatches()
+    socket.sleep(0.01)
 end
